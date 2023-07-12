3593ae34937b5318a1d253b5c52e646e149f427d
0xUgochukwu <0xUgochukwu@gmail.com>
2023-07-12 11:48:56 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,23 @@
 
 """BaseModel Class"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """A BaseModel class"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+	if kwargs:
+		del kwargs['__class__']
+
+		for key, value in kwargs:
+			if key == 'created_at' or key == 'updated_at':
+				value = datetime.fromisoformat(value)
+		setattr(cls, key, value)
+	else:
+		self.id = str(uuid.uuid4())
+        	self.created_at = datetime.now()
+        	self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -20,7 +28,7 @@ class BaseModel:
 
     def to_dict(self):
         data = self.__dict__.copy()
-        data["__class__"] = self.__class__.__name__
-        data[created_at] = self.created_at.isoformat()
-        data[updated_at] = self.updated_at.isoformat()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
         return data
