fa0dca4d588b5f69853d43649ea73577f51b19db
Stephanie <Stephaniechloe21@gmail.com>
2023-07-12 12:32:53 -0700

    task 5 error

--- models/base_model.py
+++ models/base_model.py
@@ -3,28 +3,29 @@
 """BaseModel Class"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """A BaseModel class"""
     def __init__(self, *args, **kwargs):
         if kwargs:
-            del kwargs['__class__']
-
-                for key, value in kwargs:
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.fromisoformat(value)
-                setattr(self, key, value)
+            for key, value in kwargs:
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+            setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = self.created_at
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         data = self.__dict__.copy()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,16 +4,29 @@ import json
 
 class FileStorage:
     """ FileStorage Class """
+    __file_path = 'database.json'
+    __objects = {}
 
-     __file_path = 'database.json'
-     __objects = {}
+    def all(self):
+        return self.__objects
 
-     def all(self):
-         return self.__objects
+    def new(self, obj):
+        key = obj.__class__.__name__ + '.' + obj.id  
+        self.__objects[key] = obj
 
-     def new(self, obj):
-         key = obj.__class__.__name__ + '.' + obj.id  
-         self.__objects[key] = obj
-     
-     def save(self):
-         
+    def save(self):
+        dict_objects = {}
+        for key in self.__objects:
+            dict_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w") as json_file:
+            json.dump(dict_objects, json_file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, "r") as json_file:
+                json_object = json.load(json_file)
+                #ERROR
+                for key in json_object:
+                    self.__objects[key] = eval(json_object[key]['__class__'](**json_object[key]))
+        except:
+            pass
