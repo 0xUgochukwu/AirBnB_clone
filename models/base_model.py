460f2d8f4430067935b14f2732ea686101274309
Stephanie <Stephaniechloe21@gmail.com>
2023-07-12 04:31:38 -0700

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""
-BaseModel Class
-"""
-import uuid, datetime
+
+"""BaseModel Class"""
+import uuid
+import datetime
+
 
 class BaseModel:
-	"""A BaseModel class"""
+    """A BaseModel class"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-		def __init__(self):
-	self.id = str(uuid.uuid4())
-	self.created_at = datetime.now()
-	self.updated_at = self.created_at
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
+    def save(self):
+        self.updated_at = datetime.now()
 
-
+    def to_dict(self):
+        data = self.__dict__.copy()
+        data["__class__"] = self.__class__.__name__
+        data[created_at] = self.created_at.isoformat()
+        data[updated_at] = self.updated_at.isoformat()
+        return data
