824d44f31e4ceaf650c13d42d1063173908dbeb2
0xUgochukwu <0xUgochukwu@gmail.com>
2023-07-12 04:55:46 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
                 for key, value in kwargs:
                     if key == 'created_at' or key == 'updated_at':
                         value = datetime.fromisoformat(value)
-                setattr(cls, key, value)
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
