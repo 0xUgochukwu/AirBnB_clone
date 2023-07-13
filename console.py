3a6a183328606cc1aa6e094c11cf836decde8690
Stephanie <Stephaniechloe21@gmail.com>
2023-07-13 10:22:05 -0700

    Task 7

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ import cmd
 import shlex
 from models.base_model import BaseModel
 from models import storage
+import json
+import ast
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,15 +14,22 @@ class HBNBCommand(cmd.Cmd):
     """A HBNBCommand class"""
     prompt = '(hbnb) '
 
-    def __func(self, commmand, arg):
-        args = shlex.split(arg)
+    def __class_validity(self, args):
 
+        args = shlex.split(args)
         if len(args) == 0:
             print("** class name missing **")
-            return
+            return False
 
         if args[0] not in globals().keys():
             print("** class doesn't exist **")
+            return False
+        return args
+
+    def __func(self, commmand, args):
+
+        args = self.__class_validity(args) 
+        if args is False:
             return
 
         if len(args) < 2:
@@ -33,7 +42,7 @@ class HBNBCommand(cmd.Cmd):
 
         if key not in objects.keys():
             print("** no instance found **")
-        elif commmand == "delete":
+        elif commmand == "destroy":
             del objects[key]
             storage.save()
         else:
@@ -47,25 +56,60 @@ class HBNBCommand(cmd.Cmd):
         'EOF command to exit the program'
         return True
 
-    def do_create(self, arg):
+    def do_create(self, args):
         'Creates a new BaseModel Instance'
-        if not arg:
-            print("** class name missing **")
-            return
-        if arg not in globals() or arg != "BaseModel":
-            print("** class doesn't exist **")
+        if self.__class_validity(args) is False:
             return
         obj = BaseModel()
         print(obj.id)
 
-    def do_show(self, arg):
+    def do_show(self, args):
         'Prints the string repr of an instance based on the class name and id'
-        self.__func("show", arg)
+        self.__func("show", args)
 
-    def do_delete(self, arg):
+    def do_destroy(self, args):
         'Deletes an instance based on the class name and id'
-        self.__func("delete", arg)
+        self.__func("destroy", args)
 
+    def do_all(self, args):
+        'Prints all string repr of all instances based or not on the class name'
+        if self.__class_validity(args) is False:
+            return
+        objects = storage.all()
+        objects_arr = []
+        for value in objects.values():
+            objects_arr.append(str(value))
+        print(objects_arr)
+
+    def do_update(self, args):
+        'Updates an instance based on the class name and id' 
+        args = self.__class_validity(args) 
+        storage.reload()
+        objects = storage.all()
+        key = args[0] + '.' + args[1]
+
+        if args is False:
+            return
+        elif len(args) < 2:
+            print("** instance id missing **")
+            return
+        elif key not in objects.keys():
+            print("** no instance found **")
+            return
+        elif len(args) < 3:
+            print("** attribute name missing **")
+            return
+        elif len(args) < 4:
+            print("** value missing **")
+            return
+        else:
+            attr = args[2]
+            attr_type = type(attr)
+            value = attr_type(args[3])
+            obj = vars(objects[key])
+            obj[attr] = value
+            storage.save()
+                
     def emptyline(self):
         'Does nothing'
         pass
