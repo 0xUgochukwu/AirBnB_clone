ced8e066b958086ce86ce04b31035d4dcfa18e2e
Stephanie <Stephaniechloe21@gmail.com>
2023-07-13 08:02:04 -0700

    bug fix and delete

--- console.py
+++ console.py
@@ -8,8 +8,36 @@ from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
+
     """A HBNBCommand class"""
     prompt = '(hbnb) '
+
+    def __func(self, commmand, arg):
+        args = shlex.split(arg)
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        if args[0] not in globals().keys():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        storage.reload()
+        objects = storage.all()
+        key = args[0] + '.' + args[1]
+
+        if key not in objects.keys():
+            print("** no instance found **")
+        elif commmand == "delete":
+            del objects[key]
+            storage.save()
+        else:
+            print(objects[key])
 
     def do_quit(self, arg):
         'Quit command to exit the program'
@@ -27,38 +55,16 @@ class HBNBCommand(cmd.Cmd):
         if arg not in globals() or arg != "BaseModel":
             print("** class doesn't exist **")
             return
-
         obj = BaseModel()
         print(obj.id)
 
     def do_show(self, arg):
-        'Displays string representation of a class based on its id'
+        'Prints the string repr of an instance based on the class name and id'
+        self.__func("show", arg)
 
-        args = shlex.split(arg)
-        print(globals().keys())
-
-        if len(args) == 0:
-            print("** class name missing **")
-            return
-
-        if arg[0] not in globals().keys():
-            print("** class doesn't exist **")
-            return
-
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-
-        storage.reload()
-        objects = storage.all()
-        key = args[0] + '.' + args[1]
-
-        if key not in objects.keys():
-            print("** no instance found **")
-        else:
-            print(objects[key])
-
-
+    def do_delete(self, arg):
+        'Deletes an instance based on the class name and id'
+        self.__func("delete", arg)
 
     def emptyline(self):
         'Does nothing'
