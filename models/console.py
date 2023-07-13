8d25d22278d85a4d87f3c02d67691ef816999d41
Stephanie <Stephaniechloe21@gmail.com>
2023-07-13 05:25:26 -0700

    Console.py -6

--- models/console.py
+++ models/console.py
@@ -6,7 +6,20 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """A HBNBCommand class"""
-    pass
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        'Quit command to exit the program'
+        return True
+
+    def do_EOF(self, arg):
+        'EOF command to exit the program'
+        return True
+
+    def emptyline(self):
+        'Does nothing'
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
